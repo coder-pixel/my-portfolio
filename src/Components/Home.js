144c5742d1baa7ea05e97ea059e0a307d36e3e90
sauvik <sauvikgoel@gmail.com>
2022-06-01 13:41:16 +0530

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="heroSection">
+
+            <div className="contentDiv">
+                <div className="textDiv">
+                    d
+                </div>
+                <div className="imgDiv">
+                    s
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
