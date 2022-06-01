d08af8fae5c03f31bbb462c29fd2f76a9c413dd7
coder-pixel <sauviksood@gmail.com>
2022-06-01 15:13:26 +0530

    commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,18 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="heroSection">
-
             <div className="contentDiv">
                 <div className="textDiv">
-                    d
+                    <h2>Hello, <span>I'm Sauvik!</span></h2>
+
+                    <div>
+                        <p>I'm a self-taught Front-end Web Developer and an aspiring fullstack dev.</p>
+                        <p>I build interactive websites that run across platforms & devices.</p>
+                    </div>
                 </div>
                 <div className="imgDiv">
-                    s
+                    {/* <img src="./images/DrawKit Larry Character Illustration (5).svg" alt="" /> */}
+                    <img src="./images/DrawKit Larry Character Illustration (2).svg" alt="" />
                 </div>
             </div>
         </div>
