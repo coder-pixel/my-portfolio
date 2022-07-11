d51aec4f5e433bb3808f7c540f1fec89ec215b85
coder-pixel <sauviksood@gmail.com>
2022-07-11 20:29:02 +0530

    added quickhire project

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -12,17 +12,24 @@ export const projectDetails = [
         title: "My Portfolio (This site)",
         description: "This website itself is made from React Js alongwith Framer Js for smooth animations.",
         github_link: "https://github.com/coder-pixel/my-portfolio",
-        hosted_link: "https://my-portfolio27.netlify.app/"
+        hosted_link: "https://sa27.netlify.app/"
     },
     {
         id: 3,
+        title: "QuickHire",
+        description: "`QuickHire' is a curated job board for all the daily wage workers, looking to be hired and want to have an online presence!",
+        github_link: "https://github.com/coder-pixel/job-listing-website",
+        hosted_link: "https://quickhire.netlify.app/"
+    },
+    {
+        id: 4,
         title: "React Website",
         description: "A simple portfolio website made on top of React, with various components for various sections.",
         github_link: "https://github.com/coder-pixel/react-portfolio-practice",
         hosted_link: "https://eclectic-youtiao-3eb967.netlify.app/"
     },
     {
-        id: 4,
+        id: 5,
         title: "React Projects Collection",
         description: "A bunch of other mini-projects, that I created while learning FrontEnd Dev. - Image Gallery, Color Generator, Accordion etc.",
         github_link: "https://github.com/coder-pixel/react-projects-collections",
