24cfd994e8142797620b4fda087583f7ba8853d8
coder-pixel <sauviksood@gmail.com>
2022-11-02 21:25:31 +0530

    added fitness app project link

--- src/projectDetails.js
+++ src/projectDetails.js
@@ -9,27 +9,34 @@ export const projectDetails = [
     },
     {
         id: 2,
+        title: "Fitness App",
+        description: "A simple fitness app with awesome animations to help you stay healthy.",
+        github_link: "https://github.com/coder-pixel/fitness-app",
+        hosted_link: "https://fitness-app27.netlify.app/"
+    },
+    {
+        id: 3,
         title: "My Portfolio (This site)",
         description: "This website itself is made from React Js alongwith Framer Js for smooth animations.",
         github_link: "https://github.com/coder-pixel/my-portfolio",
         hosted_link: "https://sa27.netlify.app/"
     },
     {
-        id: 3,
+        id: 4,
         title: "QuickHire",
         description: "`QuickHire' is a curated job board for all the daily wage workers, looking to be hired and want to have an online presence!",
         github_link: "https://github.com/coder-pixel/job-listing-website",
         hosted_link: "https://quickhire.netlify.app/"
     },
     {
-        id: 4,
+        id: 5,
         title: "React Website",
         description: "A simple portfolio website made on top of React, with various components for various sections.",
         github_link: "https://github.com/coder-pixel/react-portfolio-practice",
         hosted_link: "https://eclectic-youtiao-3eb967.netlify.app/"
     },
     {
-        id: 5,
+        id: 6,
         title: "React Projects Collection",
         description: "A bunch of other mini-projects, that I created while learning FrontEnd Dev. - Image Gallery, Color Generator, Accordion etc.",
         github_link: "https://github.com/coder-pixel/react-projects-collections",
